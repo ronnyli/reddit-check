74c4801a6518d60a239d963f51efcdb775a3dd86
Ronny <ronnyli91@gmail.com>
2018-09-09 21:56:40 -0400

    try to copy Reddit look

--- popup.js
+++ popup.js
@@ -82,6 +82,207 @@ function makeDisplay(redditPosts, encodedUrl, title) {
     });
 }
 
+function postHtml(index, permalink) {
+    // <div style="">
+    //     <div>
+    //         <div class="scrollerItem ky6nl9-18 ekZcUT Post t3_9eg409  b0pisp-0 bcykdr" id="t3_9eg409" tabindex="-1">
+    //             <div class="YA9IzN0YR-G5_oD5EUydl">
+    //                 <div class="_3YgWdffoKyCp7UaGAEQpoo">
+    //                     <div class="ky6nl9-4 hPvHME">
+    //                         <div class="ky6nl9-2 imqJCn">
+    //                             <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
+    //                                 <div class="_3wVayy5JvIMI67DheMYra2 dplx91-0 hsuuJr">
+    //                                     <i class="icon icon-upvote _2Jxk822qXs4DaXwsN7yyHA _39UOLMgvssWenwbRxz_iEn"></i>
+    //                                 </div>
+    //                             </button>
+    //                             <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
+    //                                 <div class="jR747Vd1NbfaLusf5bHre s1y8gf4b-0 kHKydv">
+    //                                     <i class="icon icon-downvote ZyxIIl4FP5gHGrJDzNpUC _2GCoZTwJW7199HSwNZwlHk"></i>
+    //                                 </div>
+    //                             </button>
+    //                         </div>
+    //                         <div class="ky6nl9-3 jpXmlO">
+    //                             <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
+    //                                 <div class="_2q7IQ0BUOWeEZoeAxN555e dplx91-0 hsuuJr">
+    //                                     <i class="icon icon-upvote _2Jxk822qXs4DaXwsN7yyHA"></i>
+    //                                 </div>
+    //                             </button>
+    //                             <div class="_1rZYMD_4xY3gRcSS3p8ODO" style="color: rgb(26, 26, 27);">24</div>
+    //                             <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
+    //                                 <div class="_1iKd82bq_nqObFvSH1iC_Q s1y8gf4b-0 kHKydv">
+    //                                     <i class="icon icon-downvote ZyxIIl4FP5gHGrJDzNpUC"></i>
+    //                                 </div>
+    //                             </button>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //                 <div class="_1poyrkZ7g36PawDueRza-J ky6nl9-1 hOHJXs">
+    //                     <div class="WnMeTcero48dKo501T-19">
+    //                         <div class="ky6nl9-16 iDSfbB">
+    //                             <a data-click-id="expando_open" target="_blank" href="https://arstechnica.com/...">
+    //                                 <button class="ky6nl9-6 dtQSjc" disabled="" data-click-id="expando_open" aria-expanded="false" aria-haspopup="true" aria-label="Expand content">
+    //                                     <i class="icon icon-link m5dnuu-0 fRrQun"></i>
+    //                                 </button>
+    //                             </a>
+    //                         </div>
+    //                         <div class="ky6nl9-9 kPovga" data-click-id="body">
+    //                             <div class="s5kz2p-1 gXkDPp">
+    //                                 <span class="y8HYJ-y_lTUHkQIc1mdCq">
+    //                                     <a data-click-id="body" class="SQnoC3ObvgnGjWt90zD9Z" href="/r/hackernews/comments/9eg409/.../">
+    //                                         <h2 class="s5kz2p-0 fNQhzP">NSA metadata program “consistent” with Fourth Amendment, Kavanaugh once argued</h2>
+    //                                     </a>
+    //                                 </span>
+    //                                 <div class="_1wrPey753PxLyLbB0NCEZP"></div>
+    //                                 <a class="b5szba-0 dQcRmW" href="https://arstechnica.com/..." size="3" rel="noopener noreferrer" target="_blank">arstechnica.com/tech-p...
+    //                                     <i class="icon icon-outboundLink qgDkGQIoFEpMMeNtfI0BY"></i>
+    //                                 </a>
+    //                             </div>
+    //                             <div class="_1xomvNxK4aHGoGa-YDw1Mc">
+    //                                 <span class="h5svje-0 cFQOcm">24 points</span>
+    //                                 <span class="h5svje-0 cFQOcm"> · </span>
+    //                                 <span class="h5svje-0 cFQOcm">1 comment</span>
+    //                                 <span class="h5svje-0 cFQOcm"> · </span>
+    //                                 <a href="/r/hackernews/">
+    //                                     <span class="h5svje-0 cFQOcm">r/hackernews</span>
+    //                                 </a>
+    //                                 <span class="h5svje-0 cFQOcm"> Posted by </span>
+    //                                 <a class="_2tbHP6ZydRpjI44J3syuqC s1461iz-0 fpcrlm" data-click-id="user" href="https://www.reddit.com/user/qznc_bot" target="_blank">u/qznc_bot</a>
+    //                             </div>
+    //                             <div class="_3AStxql1mQsrZuUIFP9xSg ky6nl9-10 bLCqLa">
+    //                                 <a class="s1i3ufq7-0 bsfRLa" data-click-id="subreddit" href="/r/hackernews/">r/hackernews</a>
+    //                                 <span class="s1dvci6o-0 hYPqQt" role="presentation">•</span>
+    //                                 <span class="_2fCzxBE1dlMh4OFc7B3Dun">Posted by</span>
+    //                                 <div class="wx076j-0 hPglCh">
+    //                                     <a class="_2tbHP6ZydRpjI44J3syuqC s1461iz-1 gWXVVu" href="/user/qznc_bot">u/qznc_bot</a>
+    //                                     <div id="UserInfoTooltip--t3_9eg409"></div>
+    //                                 </div>
+    //                                 <a class="_3jOxDPIQ0KaOWpzvSQo-1s" data-click-id="timestamp" href="https://www.reddit.com/r/hackernews/comments/9eg409/,,,/" id="PostTopMeta--Created--false--t3_9eg409" target="_blank" rel="nofollow noopener">4 hours ago</a>
+    //                             </div>
+    //                             <div class="ky6nl9-11 gaQWhZ"></div>
+    //                         </div>
+    //                         <div class="ky6nl9-12 kKLlzI">
+    //                             <a rel="nofollow" data-click-id="comments" data-test-id="comments-page-link-num-comments" class="_1UoeAeSRhOKSNdY_h3iS1O _1Hw7tY9pMr-T1F4P1C-xNU ky6nl9-14 bLBpaK" href="/r/hackernews/comments/9eg409/.../">
+    //                                 <i class="icon icon-comment _3ch9jJ0painNf41PmU4F9i _3DVrpDrMM9NLT6TlsTUMxC" role="presentation"></i>
+    //                                 <span class="FHCV02u6Cp2zYL0fhQPsO">1</span>
+    //                             </a>
+    //                             <div class="ky6nl9-13 cNPySB s1myzq5k-1 cJDeHo"></div>
+    //                             <div>
+    //                                 <button class="s9zeh3a-14 ecIAkj yla1lg-1 ejDVeC" aria-expanded="false" aria-haspopup="true" aria-label="more options" id="t3_9eg409-overflow-menu">
+    //                                     <i class="icon icon-menu yla1lg-2 dBJXeP"></i>
+    //                                 </button>
+    //                             </div>
+    //                             <div class="ky6nl9-16 dgWRak">
+    //                                 <a data-click-id="expando_open" target="_blank" href="https://arstechnica.com/...">
+    //                                     <button class="ky6nl9-6 dtQSjc" disabled="" data-click-id="expando_open" aria-expanded="false" aria-haspopup="true" aria-label="Expand content">
+    //                                         <i class="icon icon-link m5dnuu-0 fRrQun"></i>
+    //                                     </button>
+    //                                 </a>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+    `<div style="">
+        <div>
+            <div class="scrollerItem ky6nl9-18 ekZcUT Post ${permalink.id}  b0pisp-0 bcykdr" id="${permalink.id}" tabindex="-1">
+                <div class="YA9IzN0YR-G5_oD5EUydl">
+                    <div class="_3YgWdffoKyCp7UaGAEQpoo">
+                        <div class="ky6nl9-4 hPvHME">
+                            <div class="ky6nl9-2 imqJCn">
+                                <button class="vote-arrow" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
+                                    <div class="_3wVayy5JvIMI67DheMYra2 dplx91-0 hsuuJr">
+                                        <i class="small material-icons _2Jxk822qXs4DaXwsN7yyHA _39UOLMgvssWenwbRxz_iEn">arrow_drop_up</i>
+                                    </div>
+                                </button>
+                                <button class="vote-arrow" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
+                                    <div class="jR747Vd1NbfaLusf5bHre s1y8gf4b-0 kHKydv">
+                                        <i class="small material-icons ZyxIIl4FP5gHGrJDzNpUC _2GCoZTwJW7199HSwNZwlHk">arrow_drop_down</i>
+                                    </div>
+                                </button>
+                            </div>
+                            <div class="ky6nl9-3 jpXmlO">
+                                <button class="vote-arrow" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
+                                    <div class="_2q7IQ0BUOWeEZoeAxN555e dplx91-0 hsuuJr">
+                                        <i class="small material-icons _2Jxk822qXs4DaXwsN7yyHA">arrow_drop_up</i>
+                                    </div>
+                                </button>
+                                <div class="_1rZYMD_4xY3gRcSS3p8ODO" style="color: rgb(26, 26, 27);">${permalink.score}</div>
+                                <button class="vote-arrow" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
+                                    <div class="_1iKd82bq_nqObFvSH1iC_Q s1y8gf4b-0 kHKydv">
+                                        <i class="small material-icons ZyxIIl4FP5gHGrJDzNpUC">arrow_drop_down</i>
+                                    </div>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="_1poyrkZ7g36PawDueRza-J ky6nl9-1 hOHJXs">
+                        // RONNY WUZ HERE. HE DELETED THE DIV THAT DISPLAYS THE LINK ICON
+                        <div class="WnMeTcero48dKo501T-19">
+                            <div class="ky6nl9-9 kPovga" data-click-id="body">
+                                <div class="s5kz2p-1 gXkDPp">
+                                    <span class="y8HYJ-y_lTUHkQIc1mdCq">
+                                        <a data-click-id="body" class="SQnoC3ObvgnGjWt90zD9Z" href="/r/hackernews/comments/9eg409/.../">
+                                            <h2 class="s5kz2p-0 fNQhzP">NSA metadata program “consistent” with Fourth Amendment, Kavanaugh once argued</h2>
+                                        </a>
+                                    </span>
+                                    <div class="_1wrPey753PxLyLbB0NCEZP"></div>
+                                    <a class="b5szba-0 dQcRmW" href="https://arstechnica.com/..." size="3" rel="noopener noreferrer" target="_blank">arstechnica.com/tech-p...
+                                        <i class="icon icon-outboundLink qgDkGQIoFEpMMeNtfI0BY"></i>
+                                    </a>
+                                </div>
+                                <div class="_1xomvNxK4aHGoGa-YDw1Mc">
+                                    <span class="h5svje-0 cFQOcm">24 points</span>
+                                    <span class="h5svje-0 cFQOcm"> · </span>
+                                    <span class="h5svje-0 cFQOcm">1 comment</span>
+                                    <span class="h5svje-0 cFQOcm"> · </span>
+                                    <a href="/r/hackernews/">
+                                        <span class="h5svje-0 cFQOcm">r/hackernews</span>
+                                    </a>
+                                    <span class="h5svje-0 cFQOcm"> Posted by </span>
+                                    <a class="_2tbHP6ZydRpjI44J3syuqC s1461iz-0 fpcrlm" data-click-id="user" href="https://www.reddit.com/user/qznc_bot" target="_blank">u/qznc_bot</a>
+                                </div>
+                                <div class="_3AStxql1mQsrZuUIFP9xSg ky6nl9-10 bLCqLa">
+                                    <a class="s1i3ufq7-0 bsfRLa" data-click-id="subreddit" href="/r/hackernews/">r/hackernews</a>
+                                    <span class="s1dvci6o-0 hYPqQt" role="presentation">•</span>
+                                    <span class="_2fCzxBE1dlMh4OFc7B3Dun">Posted by</span>
+                                    <div class="wx076j-0 hPglCh">
+                                        <a class="_2tbHP6ZydRpjI44J3syuqC s1461iz-1 gWXVVu" href="/user/qznc_bot">u/qznc_bot</a>
+                                        <div id="UserInfoTooltip--${permalink.id}"></div>
+                                    </div>
+                                    <a class="_3jOxDPIQ0KaOWpzvSQo-1s" data-click-id="timestamp" href="https://www.reddit.com/r/hackernews/comments/9eg409/,,,/" id="PostTopMeta--Created--false--${permalink.id}" target="_blank" rel="nofollow noopener">4 hours ago</a>
+                                </div>
+                                <div class="ky6nl9-11 gaQWhZ"></div>
+                            </div>
+                            <div class="ky6nl9-12 kKLlzI">
+                                <a rel="nofollow" data-click-id="comments" data-test-id="comments-page-link-num-comments" class="_1UoeAeSRhOKSNdY_h3iS1O _1Hw7tY9pMr-T1F4P1C-xNU ky6nl9-14 bLBpaK" href="/r/hackernews/comments/9eg409/.../">
+                                    <i class="icon icon-comment _3ch9jJ0painNf41PmU4F9i _3DVrpDrMM9NLT6TlsTUMxC" role="presentation"></i>
+                                    <span class="FHCV02u6Cp2zYL0fhQPsO">1</span>
+                                </a>
+                                <div class="ky6nl9-13 cNPySB s1myzq5k-1 cJDeHo"></div>
+                                <div>
+                                    <button class="s9zeh3a-14 ecIAkj yla1lg-1 ejDVeC" aria-expanded="false" aria-haspopup="true" aria-label="more options" id="${permalink.id}-overflow-menu">
+                                        <i class="icon icon-menu yla1lg-2 dBJXeP"></i>
+                                    </button>
+                                </div>
+                                <div class="ky6nl9-16 dgWRak">
+                                    <a data-click-id="expando_open" target="_blank" href="https://arstechnica.com/...">
+                                        <button class="ky6nl9-6 dtQSjc" disabled="" data-click-id="expando_open" aria-expanded="false" aria-haspopup="true" aria-label="Expand content">
+                                            <i class="icon icon-link m5dnuu-0 fRrQun"></i>
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+}
+
 function buildCommentUrl(permalink) {
     var uri = new URI("comment.html")
     var query = {
