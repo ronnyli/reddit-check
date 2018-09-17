a56fb1cb52483585d67ee335a436abe06951fe7d
Ronny <ronnyli91@gmail.com>
2018-09-16 22:54:04 -0400

    update class names

--- popup.js
+++ popup.js
@@ -88,73 +88,50 @@ function postHtml(index, permalink) {
     $("#links").append(`
         <div style="">
             <div>
-                <div class="scrollerItem ky6nl9-18 ekZcUT Post ${permalink.id}  b0pisp-0 bcykdr" id="${permalink.id}" tabindex="-1">
-                    <div class="YA9IzN0YR-G5_oD5EUydl">
-                        <div class="_3YgWdffoKyCp7UaGAEQpoo">
-                            <div class="ky6nl9-4 hPvHME">
-                                <div class="ky6nl9-2 imqJCn">
-                                    <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
-                                        <div class="_3wVayy5JvIMI67DheMYra2 dplx91-0 hsuuJr">
-                                            <i class="small material-icons _2Jxk822qXs4DaXwsN7yyHA _39UOLMgvssWenwbRxz_iEn">arrow_drop_up</i>
-                                        </div>
-                                    </button>
-                                    <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
-                                        <div class="jR747Vd1NbfaLusf5bHre s1y8gf4b-0 kHKydv">
-                                            <i class="small material-icons ZyxIIl4FP5gHGrJDzNpUC _2GCoZTwJW7199HSwNZwlHk">arrow_drop_down</i>
-                                        </div>
-                                    </button>
-                                </div>
+                <div class="scrollerItem Post ${permalink.id}" id="${permalink.id}" tabindex="-1">
+                    <div class="scrollerItem-content">
+                        <div class="upvote-downvote-outer">
+                            <div class="upvote-downvote-inner">
                                 <div class="ky6nl9-3 jpXmlO">
-                                    <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
-                                        <div class="_2q7IQ0BUOWeEZoeAxN555e dplx91-0 hsuuJr">
-                                            <i class="small material-icons _2Jxk822qXs4DaXwsN7yyHA">arrow_drop_up</i>
+                                    <button class="vote-arrow" aria-label="upvote" aria-pressed="false" data-click-id="upvote">
+                                        <div class="upvote-arrow">
+                                            <i class="small material-icons upvote-arrow-icon">arrow_drop_up</i>
                                         </div>
                                     </button>
-                                    <div class="_1rZYMD_4xY3gRcSS3p8ODO" style="color: rgb(26, 26, 27);">${numToString(permalink.score)}</div>
-                                    <button class="cYUyoUM3wmgRXEHv1LlZv" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
-                                        <div class="_1iKd82bq_nqObFvSH1iC_Q s1y8gf4b-0 kHKydv">
-                                            <i class="small material-icons ZyxIIl4FP5gHGrJDzNpUC">arrow_drop_down</i>
+                                    <div class="score" style="color: rgb(26, 26, 27);">${numToString(permalink.score)}</div>
+                                    <button class="vote-arrow" aria-label="downvote" aria-pressed="false" data-click-id="downvote">
+                                        <div class="downvote-arrow">
+                                            <i class="small material-icons downvote-arrow-icon">arrow_drop_down</i>
                                         </div>
                                     </button>
                                 </div>
                             </div>
                         </div>
-                        <div class="_1poyrkZ7g36PawDueRza-J ky6nl9-1 hOHJXs">
-                            <div class="WnMeTcero48dKo501T-19">
-                                <div class="ky6nl9-9 kPovga" data-click-id="body">
-                                    <div class="s5kz2p-1 gXkDPp">
-                                        <span class="y8HYJ-y_lTUHkQIc1mdCq">
-                                            <a data-click-id="body" class="SQnoC3ObvgnGjWt90zD9Z" href="${buildCommentUrl(permalink)}">
-                                                <h2 class="s5kz2p-0 fNQhzP">${permalink.title}</h2>
+                        <div class="item-info">
+                            <div class="item-info-content">
+                                <div class="item-description" data-click-id="body">
+                                    <div>
+                                        <span class="item-title">
+                                            <a data-click-id="body" href="${buildCommentUrl(permalink)}">
+                                                <h2>${permalink.title}</h2>
                                             </a>
                                         </span>
                                         <div class="_1wrPey753PxLyLbB0NCEZP"></div>
                                     </div>
-                                    <div class="_1xomvNxK4aHGoGa-YDw1Mc">
-                                        <span class="h5svje-0 cFQOcm">${numToString(permalink.score)} points</span>
-                                        <span class="h5svje-0 cFQOcm"> · </span>
-                                        <span class="h5svje-0 cFQOcm">${numToString(permalink.comments)} comment(s)</span>
-                                        <span class="h5svje-0 cFQOcm"> · </span>
-                                        <a href="http://www.reddit.com/${permalink.subreddit}" target="_blank">
-                                            <span class="h5svje-0 cFQOcm">${permalink.subreddit}</span>
-                                        </a>
-                                        <span class="h5svje-0 cFQOcm"> Posted by </span>
-                                        <a class="_2tbHP6ZydRpjI44J3syuqC s1461iz-0 fpcrlm" data-click-id="user" href="https://www.reddit.com/user/${permalink.author}" target="_blank">u/${permalink.author}</a>
-                                    </div>
-                                    <div class="_3AStxql1mQsrZuUIFP9xSg ky6nl9-10 bLCqLa">
-                                        <a class="s1i3ufq7-0 bsfRLa" data-click-id="subreddit" href="http://www.reddit.com/${permalink.subreddit}" target="_blank">${permalink.subreddit}</a>
-                                        <span class="s1dvci6o-0 hYPqQt" role="presentation"> &middot; </span>
-                                        <span class="_2fCzxBE1dlMh4OFc7B3Dun">Posted by</span>
-                                        <div class="wx076j-0 hPglCh">
-                                            <a class="_2tbHP6ZydRpjI44J3syuqC s1461iz-1 gWXVVu" href="https://www.reddit.com/user/${permalink.author}" target="_blank">u/${permalink.author}</a>
+                                    <div class="item-source">
+                                        <a class="subreddit" data-click-id="subreddit" href="http://www.reddit.com/${permalink.subreddit}" target="_blank">${permalink.subreddit}</a>
+                                        <span role="presentation"> &middot; </span>
+                                        <span class="posted-by">Posted by</span>
+                                        <div class="user">
+                                            <a class="user-link" href="https://www.reddit.com/user/${permalink.author}" target="_blank">u/${permalink.author}</a>
                                             <div id="UserInfoTooltip--${permalink.id}"></div>
                                         </div>
-                                        <a class="_3jOxDPIQ0KaOWpzvSQo-1s" data-click-id="timestamp" href="${buildCommentUrl(permalink)}" id="PostTopMeta--Created--false--${permalink.id}" rel="nofollow noopener">${getAge(permalink.age)}</a>
+                                        <a class="age" data-click-id="timestamp" href="${buildCommentUrl(permalink)}" id="PostTopMeta--Created--false--${permalink.id}" rel="nofollow noopener">${getAge(permalink.age)}</a>
                                     </div>
-                                    <div class="ky6nl9-11 gaQWhZ"></div>
+                                    <div></div>
                                 </div>
-                                <div class="ky6nl9-12 kKLlzI">
-                                    <a rel="nofollow" data-click-id="comments" data-test-id="comments-page-link-num-comments" class="_1UoeAeSRhOKSNdY_h3iS1O _1Hw7tY9pMr-T1F4P1C-xNU ky6nl9-14 bLBpaK" href="${buildCommentUrl(permalink)}">
+                                <div class="col-right">
+                                    <a rel="nofollow" data-click-id="comments" data-test-id="comments-page-link-num-comments" class="_1UoeAeSRhOKSNdY_h3iS1O _1Hw7tY9pMr-T1F4P1C-xNU" href="${buildCommentUrl(permalink)}">
                                         <i class="tiny material-icons _3ch9jJ0painNf41PmU4F9i _3DVrpDrMM9NLT6TlsTUMxC" role="presentation">mode_comment</i>
                                         <span class="FHCV02u6Cp2zYL0fhQPsO">${numToString(permalink.comments)}</span>
                                     </a>
