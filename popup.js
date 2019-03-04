2f470c8cff539825506e6737b137834090363282
Ronny <ronnyli91@gmail.com>
2019-03-04 16:50:43 -0500

    WIP

--- popup.js
+++ popup.js
@@ -61,7 +61,10 @@ function buildCommentUrl(permalink) {
 global.buildCommentUrl = buildCommentUrl;
 
 chrome.runtime.getBackgroundPage(function (global) {
-    chrome.tabs.getSelected(null, function(tab){
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
         url_utils.getTabUrl(function(url) {
             const fake_tab = {
                 url:url,
