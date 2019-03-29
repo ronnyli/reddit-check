a00810583e936fc308aec0c4edc1711009c4bb87
ronnyli <ronnyli91@gmail.com>
2019-03-29 13:48:09 -0400

    flash badge

--- background.js
+++ background.js
@@ -121,14 +121,16 @@ function updateBadge(numPosts, tab) {
     var title = numPosts.toString() + " Results Found!"
     var text = numPosts.toString()
     var badgeColor = [236, 19, 19, 200];  // red
+    let flash = true;
     if (numPosts == 0) {
         text = '';
         title = 'Thredd';
+        flash = false;
     }
-    setBadge(title, text, badgeColor, tab)
+    setBadge(title, text, badgeColor, tab, flash);
 }
 
-function setBadge(title, text, badgeColor, tab) {
+function setBadge(title, text, badgeColor, tab, flash=false) {
     var tabId = tab.id
     chrome.browserAction.setTitle({"title": title, "tabId": tabId})
     !badgeColor || chrome.browserAction.setBadgeBackgroundColor({
@@ -139,6 +141,31 @@ function setBadge(title, text, badgeColor, tab) {
         "text": text,
         "tabId": tabId
     })
+    if (flash) {
+        flashBadge(text, tabId);
+    }
+}
+
+function flashBadge(text, tabId) {
+    chrome.storage.sync.get('disable_flashing_notification', function(settings) {
+        if (!(settings['disable_flashing_notification'])) {
+            const flashInterval = setInterval(function() {
+                setTimeout(function() {
+                    chrome.browserAction.setBadgeText({
+                        "text": text,
+                        "tabId": tabId
+                    });
+                }, 333);
+                chrome.browserAction.setBadgeText({
+                    "text": '',
+                    "tabId": tabId
+                });
+            }, 666);
+            setTimeout(function() {
+                clearInterval(flashInterval);
+            }, 333 * 6);
+        }
+    });
 }
 
 function backgroundSnoowrap() {
