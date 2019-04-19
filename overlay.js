fdf6c8cd628049d3825eb7bcdb947b18e388e39c
Ronny <ronnyli91@gmail.com>
2019-04-19 10:52:44 -0400

    overlay.js

---
+++ overlay.js
@@ -0,0 +1,23 @@
+if (document.getElementById('thredd-overlay') &&
+    document.getElementById('thredd-overlay').style.display == "none") {
+    document.getElementById('thredd-overlay').style.display = "block";
+} else if (document.getElementById('thredd-overlay')) {
+    document.getElementById('thredd-overlay').style.display = "none";
+} else {
+    let thredd_overlay = document.createElement('div');
+    thredd_overlay.id = 'thredd-overlay';
+    document.body.appendChild(thredd_overlay);
+    let thredd_dragbar = document.createElement('div');
+    let overlay = document.createElement('iframe');
+    overlay.id = 'thredd-iframe';
+    overlay.width = '525px';
+    overlay.height = "100%";
+    overlay.src = chrome.extension.getURL("popup.html");
+    style = overlay.style;
+    style.position = "fixed";
+    style.zIndex = 2147483646;
+    style.top = 0;
+    style.right = 0;
+    style.background = "#FFFFFF";
+    thredd_overlay.appendChild(overlay);
+}
