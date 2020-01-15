c5230c7a60a844be4d6e2667cec2dc2cc1ae3151
Ronny Li <ronnyli91@gmail.com>
2020-01-14 23:30:27 -0500

    no loading animation for now

--- js/page/parse/default.js
+++ js/page/parse/default.js
@@ -1,14 +1,7 @@
-// TODO: loading animation (might need to be a GIF)
-var loader = document.createElement('div');
-loader.className = 'loader';
-loader.innerText = 'Loading...';
 var num_thredd = document.querySelectorAll('.thredd_results').length;
 var arr=[];
 if (num_thredd==0) {
     var paragraph_links = document.querySelectorAll('p>a');
-    paragraph_links.forEach(elem => {
-        arr.push(elem.href);
-        elem.insertAdjacentElement('afterend', loader);
-    });
+    paragraph_links.forEach(elem => arr.push(elem.href));
 }
 arr;
--- js/page/parse/google.js
+++ js/page/parse/google.js
@@ -1,15 +1,7 @@
-// TODO: loading animation (might need to be a GIF)
-// TODO: create a new loader element for every link
-var loader = document.createElement('div');
-loader.className = 'loader';
-loader.innerText = 'Loading...';
 var num_thredd = document.querySelectorAll('.thredd_results').length;
 var arr=[];
 if (num_thredd==0) {
     var google_results = document.querySelectorAll('.r>a');
-    google_results.forEach(elem => {
-        arr.push(elem.href);
-        elem.insertAdjacentElement('afterend', loader);
-    });
+    google_results.forEach(elem => arr.push(elem.href));
 }
 arr;
